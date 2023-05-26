094fbeb9b0749ba66ca118a991948f06fcb984cc
defqd <DefQD@yandex.ru>
2023-05-27 00:12:54 +0300

    add dtos

---
+++ TaskManager/TaskManager.Application/DTOs/TodoLists/TodoListDto.cs
@@ -0,0 +1,10 @@
+﻿using TaskManager.Domain;
+
+namespace TaskManager.Application.DTOs.TodoLists
+{
+    public class TodoListDto
+    {
+        public string Name { get; set; }
+        public Todo[]? Todo { get; set; }
+    }
+}
---
+++ TaskManager/TaskManager.Application/DTOs/Todos/TodoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Application.DTOs.Todos
+{
+    public class TodoDto
+    {
+        public string Name { get; set; }
+        public bool Status { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
