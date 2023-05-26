0b56ec0ed5465ba77d6e793bd813b8bc0f793e07
defqd <DefQD@yandex.ru>
2023-05-27 00:26:51 +0300

    add mapper and mapping profiles

---
+++ TaskManager/TaskManager.Application/Profiles/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TaskManager.Domain;
+using TaskManager.Application.DTOs.TodoLists;
+using TaskManager.Application.DTOs.Todos;
+
+namespace TaskManager.Application.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Todo, TodoDto>().ReverseMap();
+            CreateMap<TodoList, TodoListDto>().ReverseMap();
+        }
+    }
+}
