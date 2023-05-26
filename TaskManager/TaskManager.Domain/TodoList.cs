cdb70350765a5098159b8cafe348d9d23f56c694
defqd <DefQD@yandex.ru>
2023-05-26 23:29:16 +0300

    refactor: delete TodoId in TodoList

--- TaskManager/TaskManager.Domain/TodoList.cs
+++ TaskManager/TaskManager.Domain/TodoList.cs
@@ -5,7 +5,6 @@ namespace TaskManager.Domain
     public class TodoList : BaseEntity
     {
         public string Name { get; set; }
-        public int TodoId { get; set; }
-        public Todo? Todo { get; set; }
+        public Todo[]? Todo { get; set; }
     }
 }
