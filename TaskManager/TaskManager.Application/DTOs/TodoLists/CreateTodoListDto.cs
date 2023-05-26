7ed8c32da10ef49e52a517b80804d1cffc77e515
defqd <DefQD@yandex.ru>
2023-05-26 23:48:27 +0300

    add TodoList dtos

---
+++ TaskManager/TaskManager.Application/DTOs/TodoLists/CreateTodoListDto.cs
@@ -0,0 +1,11 @@
+﻿using TaskManager.Domain;
+using TaskManager.Application.DTOs.Common;
+
+namespace TaskManager.Application.DTOs.TodoLists
+{
+    public class CreateTodoListDto : BaseEntityDto
+    {
+        public string Name { get; set; }
+        public Todo[]? Todo { get; set; }
+    }
+}
---
+++ TaskManager/TaskManager.Application/DTOs/TodoLists/UpdateTodoListDto.cs
@@ -0,0 +1,11 @@
+﻿using TaskManager.Application.DTOs.Common;
+using TaskManager.Domain;
+
+namespace TaskManager.Application.DTOs.TodoLists
+{
+    public class UpdateTodoListDto : BaseEntityDto
+    {
+        public string Name { get; set; }
+        public Todo[]? Todo { get; set; }
+    }
+}
