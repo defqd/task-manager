64174aef708eceb0006088e0ddec7214ffa31139
defqd <DefQD@yandex.ru>
2023-05-27 20:49:03 +0300

    feat: task manager dbcontex

---
+++ TaskManager/TaskManager.Persistence/TaskManagerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Domain;
+
+namespace TaskManager.Persistence
+{
+    public class TaskManagerDbContext : DbContext
+    {
+        public TaskManagerDbContext(DbContextOptions<TaskManagerDbContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TaskManagerDbContext).Assembly);
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
