315ae2bce093c415d1628b9c51fc2b66ff29a295
defqd <DefQD@yandex.ru>
2023-05-27 21:35:11 +0300

    feat: seeding database data

---
+++ TaskManager/TaskManager.Persistence/Configurations/ProjectConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManager.Domain;
+
+namespace TaskManager.Persistence.Configurations
+{
+    public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.HasData( new Project
+            {
+                Id = 1,
+                Name = "Test"
+            });
+        }
+    }
+}
---
+++ TaskManager/TaskManager.Persistence/Configurations/TodoConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManager.Domain;
+
+namespace TaskManager.Persistence.Configurations
+{
+    public class TodoConfiguration : IEntityTypeConfiguration<Todo>
+    {
+        public void Configure(EntityTypeBuilder<Todo> builder)
+        {
+            builder.HasData(
+
+            new Todo
+            {
+                Id = 1,
+                Name = "First",
+                StartDate = new DateTime(2023, 27, 5),
+                EndDate = new DateTime(2023, 28, 5),
+                Status = false
+            },
+            new Todo
+            {
+                Id = 2,
+                Name = "Second",
+                StartDate = new DateTime(2023, 27, 5),
+                EndDate = new DateTime(2023, 28, 5),
+                Status = false
+            }
+            );
+        }
+    }
+}
