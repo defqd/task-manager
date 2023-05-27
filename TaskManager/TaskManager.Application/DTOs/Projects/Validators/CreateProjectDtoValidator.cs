1b7d7fa41f19c9bdb2eba4d9cfe4d1048c8d231e
defqd <DefQD@yandex.ru>
2023-05-27 20:06:05 +0300

    feat: validation for create/update dtos

---
+++ TaskManager/TaskManager.Application/DTOs/Projects/Validators/CreateProjectDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace TaskManager.Application.DTOs.Projects.Validators
+{
+    public class CreateProjectDtoValidator : AbstractValidator<CreateProjectDto>
+    {
+        public CreateProjectDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed {ComparisonValue} characters.");
+        }
+    }
+}
---
+++ TaskManager/TaskManager.Application/DTOs/Todos/Validators/UpdateTodoDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace TaskManager.Application.DTOs.Todos.Validators
+{
+    public class UpdateTodoDtoValidator : AbstractValidator<UpdateTodoDto>
+    {
+        public UpdateTodoDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed {ComparisonValue} characters.");
+
+            RuleFor(x => x.StartDate)
+               .LessThan(x => x.EndDate).WithMessage("{PropertyName} must be before {ComparisonValue}");
+
+            RuleFor(x => x.EndDate)
+                .GreaterThan(x => x.StartDate).WithMessage("{PropertyName} must be after {ComparisonValue}");
+        }
+    }
+}
